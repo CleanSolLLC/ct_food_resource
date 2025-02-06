aa0cf74aaa755eb35381d1fffe2c33ac8e05ee5a
CleanSolLLC <mamark90@gmail.com>
2025-02-06 13:48:08 -0500

    Add reference to comments migrate, db

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :food_resource
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_06_183332) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_06_184436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,7 +19,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_06_183332) do
     t.bigint "food_resource_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["food_resource_id"], name: "index_comments_on_food_resource_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "counties", force: :cascade do |t|
@@ -71,6 +73,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_06_183332) do
   end
 
   add_foreign_key "comments", "food_resources"
+  add_foreign_key "comments", "users"
   add_foreign_key "food_resources", "towns"
   add_foreign_key "sessions", "users"
   add_foreign_key "towns", "counties"
